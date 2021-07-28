501172d2c8bc88074849b755c1c55438d92f996a
m-sunilkumar <sunilbhargav060@gmail.com>
2021-07-28 09:20:11 +0000

    find one and delete

--- src/app/controllers/restaurants.controller.js
+++ src/app/controllers/restaurants.controller.js
@@ -13,7 +13,12 @@ exports.findAllRestaurants = (req, res) => {
     });
 };
 
-exports.findRestaurantsByQuery = (query) => {
+exports.findRestaurantsByQuery = (req, res) => {
+  // const { } = req.query.params;
+  if(req.query.name){
+    
+  }
+
   Restaurant.findOne(query)
     .then((rest) => {
       res.status(200).send({ data: rest });
@@ -26,11 +31,23 @@ exports.findRestaurantsByQuery = (query) => {
 };
 
 exports.addRestaurant = async (req, res) => {
-  const restaurantData = req.body;
+  const restaurantData = new Restaurant(req.body);
   try {
     await restaurantData.save();
     res.status(201).send({ data: restaurantData });
   } catch (error) {
     res.status(400).json({ error: error.toString() });
+  }
+};
+
+exports.deleteRestaurant = async (req, res) => {
+  const restId = req.id;
+  try {
+    await Restaurant.findOneAndDelete({ business_id: restId });
+    res
+      .status(200)
+      .json({ message: "Restaurant Record Deleted Successfully", id: restId });
+  } catch (err) {
+    res.status(500).send({ message: "Unable to delete restaurant record" });
   }
 };
